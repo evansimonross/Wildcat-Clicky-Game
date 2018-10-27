6cd59dc9c43eb1241251fa7857583cc30fdc77fd
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-10-27 10:40:12 -0400

    Updates

---
+++ precache-manifest.b31ff1e231eb81fb6302a4105aa824bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74193567d8207ce0eb9f",
+    "url": "/Wildcat-Clicky-Game/static/js/runtime~main.74193567.js"
+  },
+  {
+    "revision": "b20bab88a01374a1a250",
+    "url": "/Wildcat-Clicky-Game/static/js/main.b20bab88.chunk.js"
+  },
+  {
+    "revision": "5b44c91536d8dde73b6a",
+    "url": "/Wildcat-Clicky-Game/static/js/1.5b44c915.chunk.js"
+  },
+  {
+    "revision": "b20bab88a01374a1a250",
+    "url": "/Wildcat-Clicky-Game/static/css/main.db57e73a.chunk.css"
+  },
+  {
+    "revision": "b520ae304a30630aacc128a565d3765c",
+    "url": "/Wildcat-Clicky-Game/index.html"
+  }
+];
